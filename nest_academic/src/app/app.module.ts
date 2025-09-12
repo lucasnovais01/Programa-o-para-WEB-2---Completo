490c254ece0bebc6fc7f112acfaa425ffdd03ece
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-09-12 18:56:47 -0300

    facilita-gente

--- nest_academic/src/app/app.module.ts
+++ nest_academic/src/app/app.module.ts
@@ -1,35 +1,56 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
 import { CidadeModule } from 'src/cidade/cidade.module';
 
 const oracledb = require('oracledb');
 
-oracledb.initOracleClient ({ 
+oracledb.initOracleClient ({
   libDir: 'D:/.Lucas Novais/oracle/client',
-})
-
+});
 
 // IMPORTANTE: OS DADOS DE @Module SÃO SENSÍVEIS !!!
 // E NÃO DEVEM SER FEITO UPLOAD DELES NO GITHUB
 
-// SEMPRE USAR este comando abaixo antes de fazer commit
-// git update-index --assume-unchanged src/app/app.module.ts
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: '',
-      host: '',
-      port: '',
-      username: '',
-      database: '',
-      password: '',
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.string().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_AUTOLOADENTITIES: Joi.boolean().default(true),
+        DATABASE_SYNCHRONIZE: Joi.boolean().default(true),
+        DATABASE_LOGGING: Joi.boolean().default(true),
+        DATABASE_ROW_NUMBER: Joi.boolean().default(true),
+      }),
+    }),
+
+    TypeOrmModule.forRootaAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'oracle',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        sid: configService.get('DATABASE_DATABASE'),
+
+        password: configService.get('DATABASE_PASSWORD'),
+        autoLoadEntities: configService.get('DATABASE_AUTOLOADENTITIES'),
+        synchronize: configService.get('DATABASE_SYNCHRONIZE'),
+        logging: ['query', 'error'],
+      }),
     }),
     CidadeModule,
   ],
 
   // IMPORTANTE: OS DADOS ACIMA, são secretos
-
 
   //  controllers: [], // Controllers handle incoming requests, é como se fosse o ponto de entrada da aplicação
   //  providers: [], //Providers are used to encapsulate business logic and can be injected into controllers or other providers,
