bdecc77c998b6354ec573bf0813bc6b2fdfdda69
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-10-03 19:43:03 -0300

    estou confuso

---
+++ README4.md
@@ -0,0 +1,13 @@
+aula 03-10-2025
+
+Foi configurando o VITE
+
+Em package.json do react_academic do professor está assim: O json não permite comentários
+  "scripts": {
+    "dev": "vite", /*vite --port 3000*/
+    "build": "tsc -b && vite build",
+    "lint": "eslint .",
+    "preview": "vite preview"
+  },
+
+
--- react_academic/eslint.config.js
+++ react_academic/eslint.config.js
@@ -1,23 +1,50 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import pluginReact from 'eslint-plugin-react'; // Importa o plugin principal do React
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    parser: tseslint.parser,
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: import.meta.dirname,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        tsx: true,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-])
+);
