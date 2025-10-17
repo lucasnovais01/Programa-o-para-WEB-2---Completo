eda87af72118d51883a2977d4c6d1f8ca31212a9
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-10-17 20:36:15 -0300

    Antes do intervalo

--- react_academic/src/services/router/url.ts
+++ react_academic/src/services/router/url.ts
@@ -1,5 +1,26 @@
 const ROTA_SISTEMA = 'sistema';
 
+export const DASHBOARD = `/${ROTA_SISTEMA}/dashboard`;
+
+
+const LISTAR = `listar`;
+const CRIAR = `criar`;
+const POR_ID = `buscar`;
+const ATUALIZAR = `atualizar`;
+const EXCLUIR = `excluir`;
+
+function gerarRotaSistema(entity: string){
+  const base = `${ROTA_SISTEMA}/${entity}`;
+  
+  return {
+    LISTAR: `/${base}/${LISTAR}`,
+    CRIAR: `/${base}/${CRIAR}`,
+    POR_ID: `/${base}/${POR_ID}`,
+    ATUALIZAR: `/${base}/${ATUALIZAR}`,
+    EXCLUIR: `/${base}/${EXCLUIR}`,
+  }
+}
+
 export const ROTA = {
 
 }
