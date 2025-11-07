3b4eba31e2d0004140155e31c15447d97a51f801
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-11-07 19:44:31 -0300

    a

--- react_academic/src/components/mensagem/MensagemErro.tsx
+++ react_academic/src/components/mensagem/MensagemErro.tsx
@@ -10,7 +10,30 @@ const MensagemErro = ({error, mensagem } : MensagemProps) {
   const unique = Array.from(
 
     new Set( typeof mensagem === "string" ? [mensagem] : mensagem || [])
+  );
+
+  return (
+    <>
+      {
+        error && unique.length > 0 && (
+          <div className="invalid-feedback">
+            {
+              unique.map((item, index) => (
+                <p key={index} style={{ margin: "0", color: "red"}}>
+                <span>
+                  {item}
+                </span>
+
+                </p>
+              ))
+            }          
+          </div>
+        )
+      }
+    </>
   )
 }
+
+export default MensagemErro;
 
 // Set é uma lista indexada, não pode ter elementos repetitidos dentro
