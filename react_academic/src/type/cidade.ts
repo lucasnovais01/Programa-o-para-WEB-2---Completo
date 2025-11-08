f7320e74e816eea89d296d2d5d16e7f95efb196c
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-11-07 21:12:57 -0300

    a

--- react_academic/src/type/cidade.ts
+++ react_academic/src/type/cidade.ts
@@ -3,3 +3,15 @@ export interface Cidade {
   codCidade?: string;
   nomeCidade?: string;
 }
+
+// O que há de novo:
+
+export interface ErrosCidade {
+  idCidade?: boolean;
+  codCidade?: boolean;
+  nomeCidade?: boolean;
+
+  idCidadeMensagem?: string[];
+  codCidadeMensagem?: string[];
+  nomeCidadeMensagem?: string[];
+}
--- react_academic/src/views/cidade/Criar.tsx
+++ react_academic/src/views/cidade/Criar.tsx
@@ -5,20 +5,126 @@ import { MdCancel } from "react-icons/md"
 
 import { CIDADE } from "../../services/cidade/constants/cidade.constants";
 
-import type { Cidade } from "../../services/cidade/type/cidade";
-
 import { apiPostCidade } from "../../services/cidade/api/api.cidade";
+
+
+import type { Cidade } from "../../services/cidade/type/cidade";
+import type { ErrosCidade } from "../../type/cidade";
 
 export default function CriarCidade() {
 
   // hook para monitorar o estado do codigo
   // assincrono
 
-  const [model, setModel] = useState<Cidade | null>(null); // ele pode ter um valor ou não pode ter nada
+  const [model, setModel] = useState<Cidade>(CIDADE.DADOS_INICIAIS); // ele pode ter um valor ou não pode ter nada
+
+// controlar o estado do erro
+  const [erros, setErrors] = useState<ErrosCidade>({});
+
 
   const handleChangeField = ( name: keyof Cidade, value: string ) => {
-    setModel((prev) => ({...prev, [name]:value }))
+    setModel((prev) => ({...prev, [name]:value }));
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: undefined,
+      [`${name}Mensagem`]:undefined, //name concatenando com a mensagem
+    }));
   };
+
+// Aquela mensagem de erro que aparece
+
+  const validateField = (
+    name: keyof Cidade, 
+    e:React.FocusEvent<HTMLInputElement>
+  ) => {
+
+    let messages: string[] = [];
+    const value = model[name];
+
+    switch(name){
+      case CIDADE.FIELDS.CODIGO:
+        if (!value) messages.push(CIDADE.INPUT_ERROR.CODIGO.BLANK);
+
+        if (value && typeof value !== "string") {
+          messages.push(CIDADE.INPUT_ERROR.CODIGO.STRING);
+        }
+          
+        break;
+
+      case CIDADE.FIELDS.NOME:
+
+        if (!value || String(value).trim().length === 0 ) {
+          messages.push(CIDADE.INPUT_ERROR.NOME.BLANK);
+        }
+
+        if (String(value).length > 0 && String(value).length) {
+          messages.push(CIDADE.INPUT_ERROR.NOME.MIN_LEN);
+        }
+        
+        if (String(value).length > 100) {
+          messages.push(CIDADE.INPUT_ERROR.NOME.MAX_LEN);
+        }
+        break;
+
+    }
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: messages.length > 0,
+      [`${name}Mensagem`]: messages.length > 0 ? messages : undefined, //name concatenando com a mensagem
+    }));
+
+    }
+  
+
+// a lógica da const validarFormulario, considere que o form está errado desde o começo
+
+  const validarFormulario = (): boolean => {
+    const newErrors : ErrosCidade = {}
+    let isFormValid = true;
+
+    const codCidadeMessages = [];
+
+    if (!model.codCidade){
+      codCidadeMessages.push(CIDADE.INPUT_ERROR.CODIGO.VALID)
+    }
+
+    if (model.codCidade && typeof model.codCidade !== "string"){
+      codCidadeMessages.push(CIDADE.INPUT_ERROR.CODIGO.STRING)
+    }
+
+    if (codCidadeMessages.length > 0){
+      newErrors.codCidade = true;
+      newErrors.codCidadeMensagem = codCidadeMessages;
+      isFormValid = false;
+    }
+
+    const nomeCidadeMessages = [];
+
+    if (!model.nomeCidade || model.nomeCidade.trim().length){
+      nomeCidadeMessages.push(CIDADE.INPUT_ERROR.NOME.BLANK)
+    }
+
+    if (model.nomeCidade) {
+      if (model.nomeCidade?.length > 0) && model.nomeCidade.length
+    }
+
+
+    if (model.nomeCidade?.length > 0 && typeof model.codCidade !== "string"){
+      nomeCidadeMessages.push(CIDADE.INPUT_ERROR.NOME.STRING)
+    }
+
+
+    if (nomeCidadeMessages.length > 0){
+      newErrors.codCidade = true;
+      newErrors.codCidadeMensagem = nomeCidadeMessages;
+      isFormValid = false;
+    }
+
+    return isFormValid;
+  };
+
 
 
   /* function getInputClass() {  modo classico, sem o arrow function */
@@ -40,7 +146,7 @@ const onSubmitForm = async (e: any) => {
   catch (error:any){
     console.log(error);
   }
-};
+
 
   return (
     <div className="display"> {/* display é uma classe global, centraliza, pois é o display flex */}
@@ -54,6 +160,7 @@ const onSubmitForm = async (e: any) => {
             <label htmlFor="codCidade" className="appLabel">
               {CIDADE.LABEL.CODIGO}
             </label>
+
             <input
               id={CIDADE.FIELDS.CODIGO}
               name={CIDADE.FIELDS.CODIGO}
@@ -62,8 +169,15 @@ const onSubmitForm = async (e: any) => {
               readOnly={false}
               disabled={false}
               autoComplete="off"
-              onChange={(e) => handleChangeField(CIDADE.FIELDS.CODIGO,e.target.value)}
+
+              // a
+
+              onChange={(e) => 
+                handleChangeField(CIDADE.FIELDS.CODIGO,e.target.value)}
+
+              onBlur = {(e) => validateField(CIDADE.FIELDS.CODIGO, e)}
               >
+
             </input> {/* appInput é uma classe global, estiliza o input */}
           </div>
 
@@ -118,5 +232,6 @@ const onSubmitForm = async (e: any) => {
         </form>
       </div>
     </div>
-  )
-}
+    )
+  };
+};
