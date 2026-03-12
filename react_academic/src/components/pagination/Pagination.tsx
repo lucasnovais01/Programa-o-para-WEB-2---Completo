6b10fe09d98fe6e617b502c5d50e6c081b5a69ce
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2026-03-11 21:20:24 -0300

    css

---
+++ react_academic/src/components/pagination/Pagination.tsx
@@ -0,0 +1,21 @@
+import './pagination.css';
+
+const Pagination = (
+  currentPage,  //1,2,3,4,5
+  totalPages,
+  onPageChange,
+
+) => {
+
+
+  return (
+    <>
+      <div>
+        Paginação.
+
+      </div>
+    </>
+  )
+}
+
+export default Pagination;
