78e2e31eb678b857b556d08ec6f68d632feca9bd
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2026-03-18 21:14:20 -0300

    ta dificil

--- react_academic/src/components/pagination/Pagination.tsx
+++ react_academic/src/components/pagination/Pagination.tsx
@@ -1,3 +1,4 @@
+import { FaArrowAltCircleLeft, FaFastBackward } from 'react-icons/fa';
 import './pagination.css';
 
 type PaginationProps = {
@@ -20,6 +21,34 @@ const Pagination = ({
   return (
     <>
       <div className='pagination'>
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <button onClick={() => onPageChange(1)} className="page-link">
+              <FaFastBackward />
+            </button>
+          </li>
+
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+
+            <button onClick={() => onPageChange(currentPage)} className='page-link'>
+              <FaArrowAltCircleLeft />
+            </button>
+          </li>
+
+          {
+            Array.from({ length: totalPages }, (_,index) => (
+              <li className={`page-item ${ currentPage === index + 1 ? 'active' : ""}`}>
+                <button onClick={() => onPageChange(index+1)} className='page-link'>
+
+                  {index + 1}
+
+                </button>
+              </li>
+            ))
+          }
+
+
+        </ul>
         Paginação.
 
       </div>
